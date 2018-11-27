1d9f0effd77faabbb6cb8a290a4ae13953f614f6
Hill, Roger <roger.hill@debeersgroup.com>
2018-11-27 15:04:33 +0000

    Updates to automate generation and build of Early bound entities assembly

---
+++ ExternalDll/GenerateEarlyBindings.cmd
@@ -0,0 +1,7 @@
+﻿REM
+REM Use CRMSVCUtil to regenerate the Early Bindings.
+
+dir
+
+..\coretools\crmsvcutil.exe /url:https://rbh2.api.crm11.dynamics.com/XRMServices/2011/Organization.svc /out:Entities.cs /username:roger@rbh2.onmicrosoft.com /password:Password1 /namespace:DynamicsLinq /serviceContextName:OrgServiceContext
+	
